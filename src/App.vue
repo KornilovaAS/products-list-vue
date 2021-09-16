1f5df3b13d5328224208269f10769b0928a7bb83
Alla Kornilova <bukareva90@mail.ru>
2021-09-17 01:04:15 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div :class="$style.wrapper">
+    <Sidebar :class="$style.sidebar" />
+    <TopPageComponent :class="$style.topPageComponent" />
+  </div>
+</template>
+
+<script>
+import Sidebar from './components/Sidebar';
+import TopPageComponent from './components/TopPageComponent';
+
+export default {
+  name: 'App',
+  components: {
+    Sidebar,
+    TopPageComponent,
+  },
+};
+</script>
+
+<style lang="scss" module>
+@import 'styles/typography.scss';
+@import 'styles/global';
+.wrapper {
+  padding-top: 30px;
+  display: grid;
+  grid-template-columns: auto 332px minmax(320px, 1440px) auto;
+  grid-template-rows: auto;
+
+  min-height: 100vh;
+  grid-template-areas: '. sidebar topPageComponent .';
+  .sidebar {
+    grid-area: sidebar;
+  }
+  .topPageComponent {
+    grid-area: topPageComponent;
+  }
+}
+</style>
---
+++ src/components/TopPageComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>TopPageComponent</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopPageComponent',
+};
+</script>
+
+<style module></style>
